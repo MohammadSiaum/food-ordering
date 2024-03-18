c7630e55ec20bb72cc7f19e5e77febf07fabf2c0
MohammadSiaum <siaumahmed013@gmail.com>
2024-03-18 23:36:57 +0600

    next auth error remove

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -10,6 +10,47 @@ import NextAuth from "next-auth/next";
 import { useSession } from "next-auth/react";
 import AppSession from '../../../../components/AppSession'
 
+const handler = NextAuth({
+  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      id: 'credentials', 
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "siam@gmail.com"},
+        password: { label: "Password", type: "password" }
+        
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
+        // console.log(credentials);
+
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({email});
+        const passwordOK = user && bcrypt.compareSync(password, user.password);
+        
+        console.log(passwordOK);
+        // console.log(user);
+        // console.log(user);
+        if (passwordOK) {
+          // console.log(user, 'auth');
+          return user;
+        }
+
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
+  ],
+
+})
+
 
 export const authOptions = {
 
@@ -57,6 +98,5 @@ export const authOptions = {
 
 // console.log(authOptions, 'authOptions');
 
-const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
